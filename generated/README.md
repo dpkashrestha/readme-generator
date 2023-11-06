870d4175e977bdb7501a1dba0d74d7b92f0861e5
dpkashrestha <dpkashrestha7@gmail.com>
2023-11-05 18:52:05 -0600

    Added initial functions to create Readme file

---
+++ generated/README.md
@@ -0,0 +1,33 @@
+# Weather Dashboard
+
+## Description 
+
+This gives you weather information.
+
+## Table of Contents 
+
+1. How to run?
+
+## Installation 
+
+None
+
+## Usage 
+
+node index.js
+
+## License 
+
+Apache License 2.0
+
+## Credits 
+
+Do not change me code!
+
+## Tests 
+
+None
+
+## Question 
+
+No question please.
--- index.js
+++ index.js
@@ -1,16 +1,129 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
-const gm = require('./utils/generateMarkdown.js');
+const gm = require("./utils/generateMarkdown.js");
+// const getHtmlPage = ()
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    message: "What is title of your Project?",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "Write description about your Project.",
+    name: "description",
+  },
+    {
+      type: "input",
+      message: "Write Table of Contents of your Project.",
+      name: "contents",
+    },
+    {
+      type: "input",
+      message: "Instalation instructions",
+      name: "install",
+    },
+    {
+      type: "input",
+      message: "Usage information",
+      name: "usage",
+    },
+    {
+      type: "list",
+      message: "License",
+      name: "license",
+      choices: ["MIT", "Apache License 2.0", "Eclipse Public License 2.0"],
+    },
+    {
+      type: "input",
+      message: "Contribution guidelines",
+      name: "contribution",
+    },
+    {
+      type: "input",
+      message: "Tests instructions",
+      name: "tests",
+    },
+    {
+      type: "input",
+      message: "Questions",
+      name: "question",
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// const getReadMePage = (response) =>
+// `"${response.title}",
+// "${response.description}"
+// "${response.contents}"
+// "${response.install}"
+// "${response.usage}"
+// "${response.license}"
+// "${response.contribution}"
+// "${response.tests}"
+// "${response.question}"
+// `;
+
+function getReadMePage(response) {
+  let content = `
+  
+  ${gm.generateMarkdown(response)}
+
+## Description 
+
+${response.description}
+
+## Table of Contents 
+
+${response.contents}
+
+## Installation 
+
+${response.install}
+
+## Usage 
+
+${response.usage}
+
+## License 
+
+${response.license}
+
+## Credits 
+
+${response.contribution}
+
+## Tests 
+
+${response.tests}
+
+## Question 
+
+${response.question}
+`;
+
+return content;
+}
+
+function writeToFile(fileName, data) {
+  const readMePageContent = getReadMePage(data);
+  console.log(readMePageContent);
+  
+  fs.writeFile("./generated/"+fileName, readMePageContent, (err) =>
+    err ? console.error(err) : console.log("Readme file created!")
+  );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then((response) => {
+    // console.log(response);
+    writeToFile("README.md", response);
+  });
+}
 
 // Function call to initialize app
 init();
